2d492a15f0681638b43e7fd751be5ee9c76e0f3b
fersarr <fersarr@gmail.com>
2014-03-06 20:06:07 -0300

    power set of a set

---
+++ problems/set_power_set.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+//Print the power set of a set. Time complexity O(2^|s|) where |s| is the size of the set
+//the recursion is doing a dfs with an element chosen and not chosen
+
+
+//power set of S is the set of S and all the subsets of S, also the empty set.
+int numbers[]={1,2,3};
+//int numbers[]={1,2,3,4};
+int setSize=sizeof(numbers)/sizeof(int);
+
+#define MAX 1000 //1000 sets of 1000 elements
+int sets[MAX][MAX];
+int setCounter;
+int printSet[MAX];
+
+
+void dfs(int index,int lowestAllowed){
+	
+	for(int i=0;i<index;i++){
+		cout<<printSet[i];
+	}
+	cout<<endl;
+	setCounter++;
+	
+	for(int i=lowestAllowed;i<setSize;i++){
+		printSet[index]=numbers[i];
+		dfs(index+1,i+1);
+	}
+	
+}
+
+int main()
+{
+
+	setCounter=0;
+	dfs(0,0);	
+	
+	//dfs(S);
+	cout<<"<empty>"<<endl;
+
+
+
+
+
+
+
+
+	return 0;
+}
